5941c0a6128b79ead713d78a0b3288ad66cf835e
Sverde-dot <55793525+Sverde-dot@users.noreply.github.com>
2020-02-11 10:29:07 +0100

    Add files via upload

--- ejemplomatriz/Ejemplomatriz.java
+++ ejemplomatriz/Ejemplomatriz.java
@@ -19,20 +19,22 @@ public class Ejemplomatriz {
     public static void main(String[] args) {
         // TODO code application logic here
         int [][] matriz = new int [3][4];
-        String[]alumnos={"alum1,alum2,alum3"};
-        String[]modulos={"Modulo1,Modulo2,Modulo3,Modulo4"};
+        String[]alumnos={"alum1","alum2","alum3"};
+        String[]modulos={"Modulo1  Modulo2  Modulo3  Modulo4  Media"};
+        //int[]mModulos= new mModulos[4];
+        //int[]mAlum = new mAlum[3];
         int option;
         Metodos obx = new Metodos();
         do{
-            option = Integer.parseInt(JOptionPane.showInputDialog("\n 1 --> crear Matriz\n 2 --> muestra \n 3 --> muestra todo \n 4 --> salir \n La opcion"));
+            option = Integer.parseInt(JOptionPane.showInputDialog("\n 1 --> crear Matriz\n 2 --> muestra todo \n 3 --> muestra media \n 4 --> salir \n La opcion"));
             switch(option){
                 case 1: matriz=obx.matriz(matriz);
                 break;
-                case 2: obx.mostra(matriz);
+                case 2: obx.mostratodo(matriz, alumnos, modulos);
                 break;
-                case 3: obx.mostratodo(matriz, alumnos, modulos);
+                case 3: obx.mediaModulos(matriz, mModulos)
                 break;
-                case 4:obx.qitar();
+                case 4: obx.qitar();
                 break;
             }
                     
--- ejemplomatriz/Metodos.java
+++ ejemplomatriz/Metodos.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package ejemplomatriz;
-import Pedir;
+import ejemplomatriz.Pedir.*;
 import java.util.Scanner;
 
 /**
@@ -19,7 +19,7 @@ public class Metodos {
     public int [][] matriz(int [][]tabla){
         for (int f =0;f<tabla.length;f++)
             for (int c =0;c<tabla[f].length;c++){
-                tabla[f][c]= num;
+                tabla[f][c]= Pedir.PedirInt();
             }
         return tabla;
     }
@@ -31,12 +31,13 @@ public class Metodos {
     }
     }
     public void mostratodo(int[][]tabla,String[]alumnos,String[]modulos){
+
         for(int m =0;m<modulos.length;m++ )
-            System.out.println(" "+modulos[m]+" ");
+            System.out.println(""+modulos[m]+"  ");
         for(int f =0;f<tabla.length;f++ ){
-            System.out.println("\n "+alumnos[f]);
-            for (int c =0;c<tabla[f].length;c++)
-                System.out.print(tabla [f][c]+" ");
+            System.out.println("\n"+alumnos[f]);
+        for (int c =0;c<tabla[f].length;c++)
+            System.out.print(tabla [f][c]+"         ");
     }
     }
     public void qitar(){
@@ -45,18 +46,38 @@ public class Metodos {
     public String[] arrayFila(int [][]tabla){
         String[]filas = new String [tabla.length];
         for(int i=0;i< filas.length;i++)
-            System.out.println("Introduce el numero");
-            filas=sc.nextInt();
+            filas[i]=Pedir.pedirString();
         return filas;
 
     }
     public String[] arrayColumnas(int [][]tabla){
         String[]columnas = new String [tabla[0].length];
         for(int i=0;i< columnas.length;i++)
-            System.out.println("Introduce el numero");
-            columnas=sc.nextInt();
-        //columnas[i]=PedirDatos.
+            columnas[i]=Pedir.pedirString();
         return columnas;
+    }
+    public int[] mediaAlum(int[][]tabla,int[]mAlum){
+        int acu=0;
+        for(int c=0;c<mAlum.length;c++){
+            acu=0;
+        for(int f=0;f<tabla.length;f++)
+            acu=acu+tabla[f][c];
+            System.out.println("-"+acu);
+            mAlum[c]=acu/mAlum.length;
+        }
+        return mAlum;
+    }
+    
+    public int[] mediaModulos(int[][]tabla,int[]mModulos){
+        int acu=0;
+        for(int c=0;c<mModulos.length;c++){
+            acu=0;
+        for(int f=0;f<tabla.length;f++)
+            acu=acu+tabla[f][c];
+            System.out.println("-"+acu);
+            mModulos[c]=acu/mModulos.length;
+        }
+        return mModulos;
     }
 }
 
